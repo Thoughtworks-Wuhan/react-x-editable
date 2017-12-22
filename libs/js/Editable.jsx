6ada07d1352a06d518aef4a0b8a4e9cda53f39c5
Baiyuan Wang <bywang@thoughtworks.com>
2017-12-22 15:10:54 +0800

    value refresh when receive new props

--- libs/js/Editable.jsx
+++ libs/js/Editable.jsx
@@ -57,13 +57,13 @@ export default class Editable extends Component {
         this.setEditable(false);
         console.log('editable', nextProps.value, this.value);
         if (nextProps.value != this.value) {
-            this.setInitialValue();
+            this.setInitialValue(nextProps.value);
         }
     }
 
-    setInitialValue = () => {
-        const { dataType, options, value } = this.props;
-        console.log('editable value', value);
+    setInitialValue = (nextValue) => {
+        let { dataType, options, value } = this.props;
+        value = nextValue || value;
         if (dataType == "select" || dataType == "checklist") {
             if (options == null) {
                 throw("Please specify options for " + dataType + " data type");
